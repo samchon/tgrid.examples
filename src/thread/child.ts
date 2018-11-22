e8a4911d81ad9c74e9a18befc456db1e3de88930
Jeongho Nam <samchon@samchon.org>
2018-11-22 23:22:59 +0900

    fake thread

---
+++ src/thread/child.ts
@@ -0,0 +1,31 @@
+import { WorkerServer } from "tgrid/protocol/worker";
+import { Driver } from "tgrid/base";
+
+import { Mutex } from "tstl/thread";
+import { randint } from "tstl/algorithm";
+
+interface IController
+{
+    mutex: Mutex;
+    print(str: string): void;
+}
+
+async function main(str: string): Promise<void>
+{
+    // PREPARE SERVER & DRIVER
+    let server: WorkerServer = new WorkerServer();
+    let driver: Driver<IController> = server.getDriver<IController>();
+
+    // REMOTE FUNCTION CALLS
+    await driver.mutex.lock();
+    {
+        for (let i: number = 0; i < 20; ++i)
+            await driver.print(str);
+        await driver.print("\n");
+    }
+    await driver.mutex.unlock();
+
+    // CLOSE THE SERVER (WORKER)
+    await server.close();
+}
+main(randint(0, 9) + "");
---
+++ src/thread/parent.ts
@@ -0,0 +1,31 @@
+import { WorkerConnector } from "tgrid/protocol/worker";
+
+import { Vector } from "tstl/container";
+import { Mutex } from "tstl/thread";
+
+// FEATURES TO PROVIDE
+namespace provider
+{
+    export var mutex = new Mutex();
+    export function print(str: string): void
+    {
+        process.stdout.write(str);
+    }
+}
+
+async function main(): Promise<void>
+{
+    let workers: Vector<WorkerConnector<typeof provider>> = new Vector();
+
+    // CREATE WORKERS
+    for (let i: number = 0; i < 4; ++i)
+    {
+        workers.push_back(new WorkerConnector(provider));
+        workers.back().connect(__dirname + "/child.js");
+    }
+
+    // WAIT THEM TO BE CLOSED
+    for (let w of workers)
+        await w.join();
+}
+main();
